e51cb1aca0d09000eec1775b793ac6ba5150751c
FelipeArango2006 <feliparango@gmail.com>
2022-05-03 17:28:16 -0500

    adsSdasd

--- controlador.js
+++ controlador.js
@@ -1,104 +1,139 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
+    nombre:"zapatillas para mujer cali dream lth",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto2={
-    nombre:"producto2",
+    nombre:"zapatillas wild rider grip",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto3={
-    nombre:"producto3",
+    nombre:"top estilo corsè con estampado integral",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto4={
-    nombre:"producto4",
+    nombre:"falda con abertura para mujer",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto5={
-    nombre:"producto5",
+    nombre:"zapatillas para mujer cali dream lth",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto6={
-    nombre:"producto6",
+    nombre:"Zapatillas para mujer cali dream lth",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto7={
-    nombre:"producto7",
+    nombre:"shorts de talle alto para mujer",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto8={
-    nombre:"producto8",
+    nombre:"caniseta holgada para mujer",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto9={
-    nombre:"producto9",
+    nombre:"vestido para mujer summer graphic",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto10={
-    nombre:"producto10",
+    nombre:"camiseta holgada para mujer",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto11={
-    nombre:"producto11",
+    nombre:"pantalones de deporte para mujer",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto12={
-    nombre:"producto12",
+    nombre:"camiseta corta para mujer essentials logo",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto13={
-    nombre:"producto13",
+    nombre:"zapatillas para mujer cali dream pastel",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto14={
-    nombre:"producto14",
+    nombre:"sudadera coera de cuello redondo para mujer",
     precio:500000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto15={
-    nombre:"producto15",
+    nombre:"zapatillas para mujer cali dream pastel",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto15={
+    nombre:"falda con abertura para mujer",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto15={
+    nombre:"camiseta corta para mujer essentials logo",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto15={
+    nombre:"zapatillas para mujer cali dream pastel",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto15={
+    nombre:"zapatillas para mujer cali dream pastel",
+    precio:500000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto15={
+    nombre:"zapatillas para mujer cali dream pastel",
     precio:500000,
     estado:"Nuevo",
     foto:""
---
+++ firebase.js
@@ -0,0 +1,22 @@
+/*
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.7.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAssIkoOTqUphlmPmg5ACKvTnVUpdoCJMc",
+    authDomain: "la-tiendita-5e7f9.firebaseapp.com",
+    projectId: "la-tiendita-5e7f9",
+    storageBucket: "la-tiendita-5e7f9.appspot.com",
+    messagingSenderId: "1093994550710",
+    appId: "1:1093994550710:web:7f6b9b239dba8e456f5d17",
+    measurementId: "G-0SXSQ0ZXYM"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
