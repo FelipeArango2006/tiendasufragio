86c941d62cd97b105a71f3bacbb4ea142f07ee62
FelipeArango2006 <feliparango2006@gmail.com>
2022-05-10 17:16:09 -0500

    oe

--- controlador.js
+++ controlador.js
@@ -2,141 +2,141 @@
 let producto1={
     nombre:"zapatillas para mujer cali dream lth",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%2010.webp?alt=media&token=7154ddee-6639-4268-9093-a617b4eccd49"
 }
 
 let producto2={
     nombre:"zapatillas wild rider grip",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%202.webp?alt=media&token=4096fdff-0a26-4765-ae8e-06ddd02a61c9"
 }
 
 let producto3={
     nombre:"top estilo corsè con estampado integral",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/camisa%20mujer.webp?alt=media&token=3071d3f4-9d61-42af-b0a5-33962e7a266f"
 }
 
 let producto4={
     nombre:"falda con abertura para mujer",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/pantaloneta.webp?alt=media&token=cfc8a3e6-7d01-4539-b9ff-93c365068754"
 }
 
 let producto5={
     nombre:"zapatillas para mujer cali dream lth",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%205.webp?alt=media&token=77da426a-250b-4b55-977d-f0e7bdab5810"
 }
 
 let producto6={
     nombre:"Zapatillas para mujer cali dream lth",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%204.webp?alt=media&token=dadc3d6e-1125-4603-a794-cdfa96d0f4ab"
 }
 
 let producto7={
     nombre:"shorts de talle alto para mujer",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/pantaloneta%202.webp?alt=media&token=b066af42-fc5c-4a1a-b7b6-010996373744"
 }
 
 let producto8={
     nombre:"caniseta holgada para mujer",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/camisa%20mujer%202.webp?alt=media&token=7d1bcdba-4e64-4e95-a350-c2e44cf9317a"
 }
 
 let producto9={
     nombre:"vestido para mujer summer graphic",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/vestido.webp?alt=media&token=f427ee74-e497-4abd-9cc9-129298c5b641"
 }
 
 let producto10={
     nombre:"camiseta holgada para mujer",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/camisa%20unisex.webp?alt=media&token=99a31a48-f4e2-4249-ad01-84229120a77f"
 }
 
 let producto11={
     nombre:"pantalones de deporte para mujer",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/pantaloneta%202.webp?alt=media&token=b066af42-fc5c-4a1a-b7b6-010996373744"
 }
 
 let producto12={
     nombre:"camiseta corta para mujer essentials logo",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/camisa%20unisex%202.webp?alt=media&token=e31a8c7c-57d5-4fdb-a426-96a586d3a403"
 }
 
 let producto13={
     nombre:"zapatillas para mujer cali dream pastel",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%206.webp?alt=media&token=839b4213-07ba-4c7a-b310-c04ad9f6e3f1"
 }
 
 let producto14={
     nombre:"sudadera coera de cuello redondo para mujer",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/sudadera.webp?alt=media&token=6cca2836-27be-4852-b8bb-1493d65e9620"
 }
 
 let producto15={
     nombre:"zapatillas para mujer cali dream pastel",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%207.webp?alt=media&token=30c6c541-cea7-46be-841d-b807d7f9872b"
 }
 
 let producto15={
     nombre:"falda con abertura para mujer",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/pantaloneta%202.webp?alt=media&token=b066af42-fc5c-4a1a-b7b6-010996373744"
 }
 
 let producto15={
     nombre:"camiseta corta para mujer essentials logo",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/camibuso.webp?alt=media&token=6ee72152-66a2-41fd-96bb-e66f40316997"
 }
 
 let producto15={
     nombre:"zapatillas para mujer cali dream pastel",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%208.webp?alt=media&token=82427a95-0351-47e3-9229-c2c753a126e0"
 }
 
 let producto15={
     nombre:"zapatillas para mujer cali dream pastel",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis%209.webp?alt=media&token=108c10da-ef35-4c63-a194-9d21fd3f008d"
 }
 
 let producto15={
     nombre:"zapatillas para mujer cali dream pastel",
     precio:500000,
-    estado:"Nuevo",
-    foto:""
+    estado:true,
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb"
 }
 
 //ARREGLO
