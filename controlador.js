b1fb55d8cc4327bd8319aa7792a358fbc8c38534
FelipeArango2006 <Feliparango2006@gmail.com>
2022-05-18 17:15:53 -0500

    ASDASD

--- controlador.js
+++ controlador.js
@@ -117,7 +117,7 @@ let producto13={
 }
 
 let producto14={
-    nombre:"sudadera coera de cuello redondo para mujer",
+    nombre:"sudadera de cuello redondo para mujer",
     precio:500000,
     estado:true,
     foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/sudadera.webp?alt=media&token=6cca2836-27be-4852-b8bb-1493d65e9620",
@@ -180,271 +180,271 @@ let producto20={
 }
 
 let producto21={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Air Force 1",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%201.webp?alt=media&token=8b1fe336-85ef-4c2a-a63f-8304a11f55ea",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto22={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Go FlyEase",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%202.webp?alt=media&token=9ef922c2-3bd1-494a-a520-5bb4eac9f3e5",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto23={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike React Infinity",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%203.webp?alt=media&token=d5b9cb2f-e1fe-46b3-aa4e-f33328fe42f8",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto24={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Air Pegasus",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%204.webp?alt=media&token=624dd966-43e5-404e-b3d5-5362e6165a30",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto25={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Blazer 77",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%205.webp?alt=media&token=01fe59ec-98e9-4bb2-96cf-be98694f8bd3",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto26={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"nike air max 270",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%206.webp?alt=media&token=9107d02d-9282-43b9-ba25-545eadb7c200",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto27={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"nike air max plus",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%207.webp?alt=media&token=a87516de-1103-4d55-934d-38677560486b",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto28={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"nike air vapormax plus",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%208.webp?alt=media&token=f865895d-bdf1-4fe8-9aaf-95b399771e7b",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto29={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"nike air 90g",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%209.webp?alt=media&token=f35ab2f0-77a0-4219-b315-24cd73562314",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto30={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Jordan XII G",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2010.webp?alt=media&token=bd366b57-785a-4d9b-b25b-0309697c9c99",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto31={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Kyrie low 5",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2011.webp?alt=media&token=4c037254-0bbf-4292-bf74-3f92c6488e3f",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto32={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Kyrie infinity",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2012.webp?alt=media&token=e63d88c5-3e55-4f90-bade-868e1da4005f",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto33={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike waffle one",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2013.webp?alt=media&token=3738329f-3d78-41a5-a55d-76f84979cfa9",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto34={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"nike air huarache",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2014.webp?alt=media&token=9dce2257-c6a0-4b22-b711-1397fe5e1766",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto35={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike 2021 FK SE",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2015.webp?alt=media&token=fe6d9383-aac5-4a04-89a6-aab99accf29b",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto36={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike 2021 FK",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2016.webp?alt=media&token=a9b196cb-d1fd-4239-b08b-a37d30d5f589",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto37={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Air max 90",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2017.webp?alt=media&token=b6d4b62a-a751-4170-b5ca-cb84d1c9c456",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto38={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"nike air max 97",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2018.webp?alt=media&token=84a77c69-f552-4ab9-a075-4f07b95a01a3",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto39={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"LeBron 19",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2019.webp?alt=media&token=13cb4589-84c8-4de9-b27d-de652b649fd2",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto40={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"LeBron 19 low",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2020.webp?alt=media&token=23c75117-448d-43f6-8e83-c6f7d2766e76",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto41={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Zoom freak 3",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2021.webp?alt=media&token=3059986b-f477-41b7-9b86-95322b035ca7",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto42={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Zion 1 SP",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2022.webp?alt=media&token=b4055aa1-fd92-4614-a340-19d3e5c844c2",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto43={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Air Force 1 '07",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2023.webp?alt=media&token=f2e65341-8ce2-4c10-aeab-a7441101b486",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto44={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Air Force 1 '07",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2024.webp?alt=media&token=7af84ec2-6323-4163-90b1-8a8e46621016",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto45={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike Air Max 90",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2025.webp?alt=media&token=26b34f83-50a3-43b1-8edb-881660d62546",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto46={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike SB zoom",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2026.jfif?alt=media&token=7c48e2fd-f6d7-465a-84a8-1d90d96e93f5",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto47={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike ZoomX",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2027.webp?alt=media&token=944fc8f0-aeec-44df-951e-209387847f97",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto48={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike React",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/nike%2028.webp?alt=media&token=29bed2b9-a272-4605-987c-e38038640770",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto49={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike vapor edge pro",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/guayos%201.webp?alt=media&token=3799e961-ba1d-49e9-96fe-0fef9f03915b",
     popularidad:"★★★★★",
     pais:"colombia"
 }
 
 let producto50={
-    nombre:"zapatillas para mujer cali dream pastel",
+    nombre:"Nike vapor edge Flyknit",
     precio:500000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/tennis.webp?alt=media&token=ade238cf-2f86-4c47-844d-449b2b8bd6bb",
+    foto:"https://firebasestorage.googleapis.com/v0/b/la-tiendita-5e7f9.appspot.com/o/guayos%202.webp?alt=media&token=ffe1a5c8-244a-4d3b-a665-4eac26b1f985",
     popularidad:"★★★★★",
     pais:"colombia"
 }
@@ -473,6 +473,7 @@ let productos=Array(
     producto20,
     producto21,
     producto22,
+    producto23,
     producto24,
     producto25,
     producto26,
